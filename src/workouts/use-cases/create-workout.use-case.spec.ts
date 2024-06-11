6ea27aa4fdd4f8093b6cc7162177ea3a5df6727f
Mathenriques <math.marqui@gmail.com>
2024-06-11 11:25:26 -0300

    Finalizando testes de criação de workout

--- src/workouts/use-cases/create-workout.use-case.spec.ts
+++ src/workouts/use-cases/create-workout.use-case.spec.ts
@@ -21,9 +21,10 @@ describe('Create Workout Use Case Test', () => {
   });
 
   it('Should be able to create an new workout', async () => {
+    const today = new Date();
     const workoutData = {
       start_date: new Date(),
-      end_date: new Date(),
+      end_date: new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000), // Define o end date para 7 dias depois do start date
       weekly_volume: 50,
       strengthening_workouts: 3,
       stress_level: 5,
@@ -37,5 +38,125 @@ describe('Create Workout Use Case Test', () => {
 
     expect(start_date).toEqual(workoutData.start_date);
     expect(end_date).toEqual(workoutData.end_date);
+  });
+
+  it('Should not be able to create an new workout with a duplicated date', async () => {
+    const today = new Date();
+    const workoutData = {
+      start_date: new Date(),
+      end_date: new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000), // Define o end date para 7 dias depois do start date,
+      weekly_volume: 50,
+      strengthening_workouts: 3,
+      stress_level: 5,
+      sleep_hours: 8,
+      pain_discomfort: '',
+      didMyofascialRelease: true,
+      user_id: 'XXX',
+    };
+
+    await useCase.execute(workoutData);
+
+    await expect(() => useCase.execute(workoutData)).rejects.toBeInstanceOf(
+      Error,
+    );
+  });
+
+  it('Should not be able to create an new workout with end date between start and end date that exists', async () => {
+    const today = new Date();
+    const workoutData = {
+      start_date: new Date(),
+      end_date: new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000), // Define o end date para 7 dias depois do start date,
+      weekly_volume: 50,
+      strengthening_workouts: 3,
+      stress_level: 5,
+      sleep_hours: 8,
+      pain_discomfort: '',
+      didMyofascialRelease: true,
+      user_id: 'XXX',
+    };
+
+    const workoutDuplicatedData = {
+      start_date: new Date(today.getTime() - 2 * 24 * 60 * 60 * 1000), // Define o start date para antes de ontem
+      end_date: new Date(today.getTime() + 2 * 24 * 60 * 60 * 1000), // Define o end date para depois de amanhã
+      weekly_volume: 50,
+      strengthening_workouts: 3,
+      stress_level: 5,
+      sleep_hours: 8,
+      pain_discomfort: '',
+      didMyofascialRelease: true,
+      user_id: 'XXX',
+    };
+
+    await useCase.execute(workoutData);
+
+    await expect(() =>
+      useCase.execute(workoutDuplicatedData),
+    ).rejects.toBeInstanceOf(Error);
+  });
+
+  it('Should not be able to create an new workout with start date between start and end date that exists', async () => {
+    const today = new Date();
+    const workoutData = {
+      start_date: new Date(),
+      end_date: new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000), // Define o end date para 7 dias depois do start date,
+      weekly_volume: 50,
+      strengthening_workouts: 3,
+      stress_level: 5,
+      sleep_hours: 8,
+      pain_discomfort: '',
+      didMyofascialRelease: true,
+      user_id: 'XXX',
+    };
+
+    const workoutDuplicatedData = {
+      start_date: new Date(today.getTime() + 2 * 24 * 60 * 60 * 1000), // Define o start date para depois de amanhã
+      end_date: new Date(today.getTime() + 8 * 24 * 60 * 60 * 1000), // Define o end date para 7 dias depois do start date
+      weekly_volume: 50,
+      strengthening_workouts: 3,
+      stress_level: 5,
+      sleep_hours: 8,
+      pain_discomfort: '',
+      didMyofascialRelease: true,
+      user_id: 'XXX',
+    };
+
+    await useCase.execute(workoutData);
+
+    await expect(() =>
+      useCase.execute(workoutDuplicatedData),
+    ).rejects.toBeInstanceOf(Error);
+  });
+
+  it('Should not be able to create an new workout where days beetwen start and end date are in another workout ', async () => {
+    const today = new Date();
+    const workoutData = {
+      start_date: new Date(),
+      end_date: new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000), // Define o end date para 7 dias depois do start date,
+      weekly_volume: 50,
+      strengthening_workouts: 3,
+      stress_level: 5,
+      sleep_hours: 8,
+      pain_discomfort: '',
+      didMyofascialRelease: true,
+      user_id: 'XXX',
+    };
+
+    const workoutDuplicatedData = {
+      start_date: new Date(today.getTime() - 2 * 24 * 60 * 60 * 1000), // Define o start date para antes de ontem
+      end_date: new Date(today.getTime() + 8 * 24 * 60 * 60 * 1000), // Define o end date para 7 dias depois do start date
+      weekly_volume: 50,
+      strengthening_workouts: 3,
+      stress_level: 5,
+      sleep_hours: 8,
+      pain_discomfort: '',
+      didMyofascialRelease: true,
+      user_id: 'XXX',
+    };
+
+    await useCase.execute(workoutData);
+
+    await expect(() =>
+      useCase.execute(workoutDuplicatedData),
+    ).rejects.toBeInstanceOf(Error);
   });
 });
